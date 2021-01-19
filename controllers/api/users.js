711db2ab92090d5fa460747d192223d4d8735aeb
Jang <sbtan1989@gmail.com>
2021-01-19 15:10:04 -0800

    infrastructure-part-5

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,27 @@
+const User = require('../../models/user');
+const jwt = require('jsonwebtoken');
+
 module.exports = {
   create
 };
 
-function create(req, res) {
-  // Baby step...
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email
-    }
-  });
-}
+async function create(req, res) {
+  try {
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    
+    res.json(token)
+  } catch (error) {
+    
+    res.status(400).json(error);
+  }
+};
+
+// jwt helper function
+function createJWT(user) {
+  return jwt.sign(
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h'}
+  );
+};
